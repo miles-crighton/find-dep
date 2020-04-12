2b21c4d7bdce02c7b7d681d2d91a155771333b5d
Miles Crighton <miles.crighton@gmail.com>
2020-04-12 13:09:12 +0100

    Setup yargs

--- src/cli.js
+++ src/cli.js
@@ -2,6 +2,21 @@
 const path = require("path");
 const fileHandler = require("./fileHandler.js");
 
-const deps = fileHandler.getDependencies();
-const packages = fileHandler.getPackageData();
-console.log("Your package deps: ", deps);
+let argv = require("yargs")
+    .usage("$0 <package> [args]")
+    .command("* <package>", "Search for a package", (yargs) => {
+        yargs.positional("package", {
+            describe: "Name of package to find",
+            type: "string",
+        });
+    }).argv;
+
+if (argv.package) {
+    console.log("Searching for package:", argv.package);
+
+    const deps = fileHandler.getDependencies();
+    const packages = fileHandler.getPackageData();
+    console.log("Your local deps: ", deps);
+} else {
+    console.log("Please provide a package string");
+}
