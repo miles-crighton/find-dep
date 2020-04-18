cdfe3b3c3bf8dddf2d3621cde7d1a86c532d9192
Miles Crighton <miles.crighton@gmail.com>
2020-04-18 10:42:29 +0100

    New tests

--- test/fileHandler.js
+++ test/fileHandler.js
@@ -2,29 +2,44 @@ var fileHandler = require("../src/fileHandler");
 var expect = require("chai").expect;
 var path = require("path");
 
-const TEST_SET1 = path.join(__dirname, "test_set1");
+const TEST_SETS = [];
+TEST_SETS.push(path.join(__dirname, "test_sets/test_set1"));
+
+TEST_SETS_EXPECTED = {};
+TEST_SETS_EXPECTED["TEST_SET1"] = {
+    dependencies: {
+        ms: "^2.1.2",
+    },
+    packages: {
+        ms: {
+            version: "2.1.2",
+            resolved: "https://registry.npmjs.org/ms/-/ms-2.1.2.tgz",
+            integrity:
+                "sha512-sGkPx+VjMtmA6MX27oA4FBFELFCZZ4S4XqeGOXCv68tT+jb3vk/RyaKWP0PTKyWtmLSM0b+adUTEvbs1PEaH2w==",
+        },
+    },
+};
 
 describe("#getDependencies()", function () {
-    context("using test_set1", function () {
-        it('Should return { ms: "^2.1.2" }', function () {
-            expect(fileHandler.getDependencies(true, TEST_SET1)).to.eql({
-                ms: "^2.1.2",
+    for (let i = 0; i < TEST_SETS.length; i++) {
+        context(`using test_set${i + 1}`, function () {
+            it("Should return correct package dependencies", function () {
+                expect(fileHandler.getDependencies(true, TEST_SETS[i])).to.eql(
+                    TEST_SETS_EXPECTED[`TEST_SET${i + 1}`].dependencies
+                );
             });
         });
-    });
+    }
 });
 
 describe("#getPackageData()", function () {
-    context("using test_set1", function () {
-        it("Should return package_lock contents", function () {
-            expect(fileHandler.getPackageData(TEST_SET1)).to.eql({
-                ms: {
-                    version: "2.1.2",
-                    resolved: "https://registry.npmjs.org/ms/-/ms-2.1.2.tgz",
-                    integrity:
-                        "sha512-sGkPx+VjMtmA6MX27oA4FBFELFCZZ4S4XqeGOXCv68tT+jb3vk/RyaKWP0PTKyWtmLSM0b+adUTEvbs1PEaH2w==",
-                },
+    for (let i = 0; i < TEST_SETS.length; i++) {
+        context(`using test_set${i + 1}`, function () {
+            it("Should return package_lock contents", function () {
+                expect(fileHandler.getPackageData(TEST_SETS[i])).to.eql(
+                    TEST_SETS_EXPECTED[`TEST_SET${i + 1}`].packages
+                );
             });
         });
-    });
+    }
 });
--- test/search.js
+++ test/search.js
@@ -1,12 +1,32 @@
-var sum = require("../src/search");
+var search = require("../src/search");
 var expect = require("chai").expect;
+const path = require("path");
 
-describe("#DFS()", function () {
-    // context("without arguments", function () {
-    //     it("should return 0", function () {
-    //         expect(sum()).to.equal(0);
-    //     });
-    // });
+const TEST_SETS = [];
+TEST_SETS.push(path.join(__dirname, "test_sets/test_set1"));
+
+describe("#search.DFS()", function () {
+    context("without arguments", function () {
+        it("should return []", function () {
+            expect(search.DFS()).to.eql([]);
+        });
+    });
+    context("core dependency as target", function () {
+        it("should return that core dep as the path [{ name, version }]", function () {
+            expect(
+                search.DFS({ targetName: "test", version: null }, "test", {
+                    test: { version: "1.0.0" },
+                })
+            ).to.eql([
+                [
+                    {
+                        name: "test",
+                        version: "1.0.0",
+                    },
+                ],
+            ]);
+        });
+    });
     // context("with number arguments", function () {
     //     it("should return sum of arguments", function () {
     //         expect(sum(1, 2, 3, 4, 5)).to.equal(15);
@@ -22,4 +42,24 @@ describe("#DFS()", function () {
     //         }).to.throw(TypeError, "sum() expects only numbers.");
     //     });
     // });
+});
+
+describe("#search.for()", function () {
+    context("without arguments", function () {
+        it("should return []", function () {
+            expect(search.for()).to.eql([]);
+        });
+    });
+    context("core dependency as target", function () {
+        it("#TEST_SET1: should return that core dep as path", function () {
+            expect(search.for({ targetName: "ms" }, TEST_SETS[0])).to.eql([
+                [
+                    {
+                        name: "ms",
+                        version: "2.1.2",
+                    },
+                ],
+            ]);
+        });
+    });
 });
